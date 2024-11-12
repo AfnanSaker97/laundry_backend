916e10effc8b2ec46cd6779ac06279e5017e7a9d
AfnanSaker <afnanoo12345@gmail.com>
2024-11-12 01:49:22 -0800

    edit echo.js

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -5,17 +5,10 @@ window.Pusher = Pusher;
 
 window.Echo = new Echo({
     broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    key: 'bdyibm6siappm5afxfgc',
+    wsHost: 'laundry-backend-test.tecrek.com',
+    wsPort: 443,
+    wssPort: 443,
+    forceTLS: true,
     enabledTransports: ['ws', 'wss'],
-});
-
-console.log("Reverb WebSocket configuration:", {
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    host: import.meta.env.VITE_REVERB_HOST,
-    port: import.meta.env.VITE_REVERB_PORT,
-    scheme: import.meta.env.VITE_REVERB_SCHEME,
 });
