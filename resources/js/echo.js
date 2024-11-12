fd1b7cd07774330e4153c4d0d3a58ecebc378a99
AfnanSaker <afnanoo12345@gmail.com>
2024-11-12 04:36:15 -0800

    edit echo.js

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -5,10 +5,12 @@ window.Pusher = Pusher;
 
 window.Echo = new Echo({
     broadcaster: 'reverb',
-    key: 'bdyibm6siappm5afxfgc',
-    wsHost: 'laundry-backend-test.tecrek.com',
-    wsPort: 443,
-    wssPort: 443,
-    forceTLS: true,
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
 });
+
+
